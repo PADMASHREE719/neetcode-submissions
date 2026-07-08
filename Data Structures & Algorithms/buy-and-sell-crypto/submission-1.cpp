eda2229f3e1d79fa9fcf763784d0e282402fc4eb
PADMASHREE719 <padmashreejain.cs23@bmsce.ac.in>
2026-07-08 21:50:53 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int min_price = INT_MAX;
+        int max_profit = INT_MIN;
+
+        for(int i=0;i<prices.size();i++){
+            if(prices[i] < min_price){
+                min_price = prices[i];
+            }
+            else{
+                max_profit = max(max_profit,prices[i]-min_price);
+            }
+        }
+        return  max_profit == INT_MIN ? 0 : max_profit;
+    }
+};
