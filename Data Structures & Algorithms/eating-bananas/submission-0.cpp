601741fe9ca1a32fc5a26cfabbd8ba2101910055
PADMASHREE719 <padmashreejain.cs23@bmsce.ac.in>
2026-08-18 15:59:29 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    int findMax(vector<int>& piles){
+        int maxi=INT_MIN;
+        for(int i=0;i<piles.size();i++){
+            maxi=max(maxi,piles[i]);
+        }
+        return maxi;
+    }
+
+    int calculatehrs(vector<int>&piles,int mid){
+        int tothrs=0;
+        for(int i=0;i<piles.size();i++){
+            tothrs+=ceil((double)piles[i]/(double)mid);
+        }
+        return tothrs;
+
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1, high=findMax(piles);
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            int totalhrs=calculatehrs(piles,mid);
+
+            if(totalhrs<=h){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+        
+    }
+};
