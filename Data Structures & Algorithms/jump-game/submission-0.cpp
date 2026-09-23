c3b6581fb2a194bf6522f8174a013c07357681db
PADMASHREE719 <padmashreejain.cs23@bmsce.ac.in>
2026-09-23 14:44:37 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIdx=0;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            if(maxIdx < i){
+                return false;
+            }
+            maxIdx=max(maxIdx,i+nums[i]);
+        }
+        return true;
+        
+    }
+};
